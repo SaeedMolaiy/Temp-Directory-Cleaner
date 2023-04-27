6ac247e4ae3ed63566b5c3752db9cba4c7c2d18e
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-04-27 01:46:17 -0700

    Create DirectoryCleaner.cs

---
+++ DirectoryCleaner.cs
@@ -0,0 +1,61 @@
+﻿namespace TempFolderCleaner;
+
+public static class DirectoryCleaner
+{
+    public static async Task CleanTempFolderAsync()
+    {
+        var tempFolder = Path.GetTempPath();
+
+        var directory = new DirectoryInfo(tempFolder);
+
+        foreach (var file in directory.EnumerateFiles())
+        {
+            try
+            {
+                void DeleteFunction() => file.Delete();
+
+                await Task.Run(DeleteFunction);
+            }
+            catch
+            {
+                Console.WriteLine("Unable to delete a file");
+            }
+        }
+
+        foreach (var subDirectory in directory.EnumerateDirectories())
+        {
+            await DeleteDirectoryAsync(subDirectory);
+        }
+    }
+
+    private static async Task DeleteDirectoryAsync(DirectoryInfo directory)
+    {
+        foreach (var file in directory.EnumerateFiles())
+        {
+            try
+            {
+                void DeleteFunction() => file.Delete();
+
+                await Task.Run(DeleteFunction);
+            }
+            catch
+            {
+                Console.WriteLine("Unable to delete a file");
+            }
+        }
+
+        foreach (var subDirectory in directory.EnumerateDirectories())
+        {
+            await DeleteDirectoryAsync(subDirectory);
+        }
+
+        try
+        {
+            await Task.Run(directory.Delete);
+        }
+        catch
+        {
+            Console.WriteLine("Unable to delete a directory");
+        }
+    }
+}
